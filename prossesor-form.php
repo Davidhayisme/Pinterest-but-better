e654da61937dfc50adfa057f3d3789ea8da56729
Elenaengelese <113140999+Elenaengelese@users.noreply.github.com>
2022-11-28 08:19:47 -0500

    Update prossesor-form.php

--- prossesor-form.php
+++ prossesor-form.php
@@ -15,11 +15,11 @@ $conn = mysqli_connect($host,$username,$password,$dbname);
 if(mysqli_connect_errno()){
     die("Connection error: " . mysqli_connect_error());
 }
-$sql = "INSERT INTO message (name,description,aesthetic,file_name) VALUES(?,?,?,?)";
+$sql = "INSERT INTO demo (name,description,aesthetic,file_name) VALUES(?,?,?,?)";
 $stmt = mysqli_stmt_init($conn);
 if(! mysqli_stmt_prepare($stmt,$sql)){
     die(mysqli_error($conn));
 }
-mysqli_stmt_bind_param($stmt,"ssii", $name,$message,$aesthetic,$type);
+mysqli_stmt_bind_param($stmt,"ssii", $name,$description,$aesthetic,$file_name);
 mysqli_stmt_execute($stmt);
 echo "Record saved";

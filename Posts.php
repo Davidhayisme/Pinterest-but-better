e315dbf21296c890b96c82346aa2deda08e20a44
Elenaengelese <113140999+Elenaengelese@users.noreply.github.com>
2022-12-08 00:12:53 -0500

    Update Posts.php

--- Posts.php
+++ Posts.php
@@ -17,59 +17,23 @@ if(isset($_POST['submit'])){
     $location = $_POST["location"];
     $rating = filter_input(INPUT_POST, "rating", FILTER_VALIDATE_INT);
     $aesthetic = filter_input(INPUT_POST, "aesthetic", FILTER_VALIDATE_INT);
-
-
-$statusMsg = '';
-    // $aesthetic_name = "";
-    // if ($aesthetic == 1):
-    //     $aesthetic_name = "Indie";
-    // if ($aesthetic_value == 2):
-    //     $aesthetic_name = "Grunge";
-    // if ($aesthetic_value == 3):
-    //     $aesthetic_name = "Soft";
-    // if ($aesthetic_value == 4):
-    //     $aesthetic_name = "Dark & light academia";
-    // if ($aesthetic_value == 5):
-    //     $aesthetic_name = "Baddie";
-    // if ($aesthetic_value == 6):
-    //     $aesthetic_name = "VISCO";
-    // if ($aesthetic_value == 7):
-    //     $aesthetic_name = "EMO";
-    // if ($aesthetic_value == 8):
-    //     $aesthetic_name = "Fairycore";
-    // if ($aesthetic_value == 9):
-    //     $aesthetic_name = "Goth";
-    // if ($aesthetic_value == 10):
-    //     $aesthetic_name = "Cottagecore";
-    // if ($aesthetic_value == 11):
-    //     $aesthetic_name = "Y2K";
-    // if ($aesthetic_value == 12):
-    //     $aesthetic_name = "90's";
-    // if ($aesthetic_value == 13):
-    //     $aesthetic_name = "80's";
-    // if ($aesthetic_value == 14):
-    //     $aesthetic_name = "70's";
-    // if ($aesthetic_value == 15):
-    //     $aesthetic_name = "60's";
-    // if ($aesthetic_value == 16):
-    //     $aesthetic_name = "50's";
-    // if ($aesthetic_value == 17):
-    //     $aesthetic_name = "40's";
-    // if ($aesthetic_value == 18):
-    //     $aesthetic_name = "30's";
-    // if ($aesthetic_value == 19):
-    //     $aesthetic_name = "20's";
-
-    $sql = "INSERT INTO posts (file_num,username,aesthetic_name,caption,store_name,location,rating) VALUES(?,?,?,?,?,?,?)";
-        $allowed = array('jpg','jpeg','png','pdf');
-        if(in_array($fileActualExt,$allowed)){
+    $sql = "INSERT INTO posts (file_num,username,aesthetic,caption,store_name,location,rating) VALUES(?,?,?,?,?,?,?)";
+    $fileNameNew = uniqid("",true).".".$fileActualExt;
+    if (in_array($fileActualExt, $allowed)){
+        if($fileError == 0){
+            if ($fileSize < 1000000){
+                $fileDestination = '/var/www/html/Photos/';
+                $stmt = mysqli_stmt_init($conn);
+                if(! mysqli_stmt_prepare($stmt,$sql)){
+                    die(mysqli_error($conn));
+                }
+                mysqli_stmt_bind_param($stmt,"isisssi", $fileNameNew, $username, $aesthetic,$caption, $store_name, $location, $rating);
+                if(mysqli_stmt_execute($stmt) && move_uploaded_file($fileTmpName,$fileDestination.$fileNameNew)){
+                    header("Location: preHomePage.php");
+                }
+            }
         }
-    mysqli_stmt_bind_param($stmt,"ssii", $file_num, $username, $aesthetic,$caption, $store_name, $location, $rating);
-    mysqli_stmt_execute($stmt);
-    $stmt = mysqli_stmt_init($conn);
-    if(! mysqli_stmt_prepare($stmt,$sql)){
-        die(mysqli_error($conn));
     }
-    echo "saved";
+    echo "not saved";
 }
 ?>

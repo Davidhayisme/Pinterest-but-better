6ab9488a44ae349487c56c6a90571ffb5ee9dc1a
Elenaengelese <113140999+Elenaengelese@users.noreply.github.com>
2022-12-09 02:14:04 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,14 +1,20 @@
 <?php
+//Submit button that will upload post
 if(isset($_POST['submit'])){
+    //posting attributes
     $file = $_FILES['file'];
     $fileName = $_FILES['file']['name'];
     $fileTmpName = $_FILES['file']['tmp_name'];
     $fileSize = $_FILES['file']['size'];
     $fileError = $_FILES['file']['error'];
     $fileType = $_FILES['file']['type'];
+    //Turns into an array of two different things plit by "."
     $fileExt = explode('.', $fileName);
+    //Checks the existence of file moving the pointer to end of array
     $fileActualExt = strtolower(end($fileExt));
     $allowed = array('jpg','jpeg','png','pdf');
+    
+    //Checking for if the photo is allowed type and within range
     if(in_array($fileActualExt,$allowed)){
         if($fileError == 0){
             if($fileSize < 1000000){

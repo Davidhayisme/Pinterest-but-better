3c3357efd82d67e303a68483205cdb2bb4d418fb
David Hay <113220253+Davidhayisme@users.noreply.github.com>
2022-12-09 06:39:26 -0500

    Documented filterposts.php

--- filterposts.php
+++ filterposts.php
@@ -1,20 +1,26 @@
 <?php
+//Calls current session
 session_start();
+//Database call
 $conn = require __DIR__ . "/connection.php";
+//Variable that distinguishes which filter was chosen
 $filternum = $_POST["filter"];
-echo "here";
+//Query to check which posts fit our aesthetic
 $sql = "SELECT file_num FROM posts WHERE aesthetic = '" . $filternum . "'";
-echo "     here";
+//Calling the query with the connection
 $result = mysqli_query($conn, $sql);
-echo "    here";
+//Creating an temp array and counter
 $otherarr = array();
-echo "    here";
 $num = 0;
+//Calls while there are still rows that fit aesthetic
 while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-    echo "here";
+    //Puts the filename into the temp array and increment counter
     $otherarr[$num] = $row["file_num"];
     $num++;
 }
+//Randomize with shuffle function
 shuffle($otherarr);
+//Set temp array into a session variable
 $_SESSION["photos"] = $otherarr;
+//Go to the home page
 header("Location: homePage.php");

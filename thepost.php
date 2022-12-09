9f15e623e9896f2712b663d3a7b115afd86c1a84
Elenaengelese <113140999+Elenaengelese@users.noreply.github.com>
2022-12-09 06:23:23 -0500

    Update thepost.php

--- thepost.php
+++ thepost.php
@@ -1,13 +1,21 @@
 <?php
+//begins session, which is away to store information across multiple pages
 session_start();
+//obtains currec7nt code working directory
 $conn = require __DIR__ . "/connection.php";
+//Variables to initiate photo variables
 $tempnum = $_POST["photo"];
 $imgarr = $_SESSION["photos"];
 $filenum = $imgarr[$tempnum];
+//variable in mysql that store unique id for the photo tag
 $sql = "SELECT * FROM posts WHERE file_num= '" . $filenum . "'";
+//runs multiple sql statements in 1 query
 $result = mysqli_query($conn, $sql);
+//if something is stored but the user is not trying to submit multiple photos
 if(mysqli_num_rows($result) === 1){
+    //associative array representing fetched rows
     $row = mysqli_fetch_assoc($result);
+    //store information in session variables
     $_SESSION["pusername"] = $row["username"];
     $_SESSION["pcaption"] = $row["caption"];
     $_SESSION["pphoto"] = $row["file_num"];

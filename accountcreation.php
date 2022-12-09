f4e4f195923db1429a47cb2a3a8c3b2b96c4f0ba
audreyviv <112990611+audreyviv@users.noreply.github.com>
2022-12-09 06:39:16 -0500

    Update accountcreation.php

--- accountcreation.php
+++ accountcreation.php
@@ -4,6 +4,7 @@
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"> 
 <style>
+        /* Header: Hidden Treasures */
         h1 {
             color: #eeeeee;
             font-size: 30px;
@@ -20,10 +21,13 @@
             padding: 60px;
         }
 
+        /* label format */
         label,p {
             font-family: Georgia, serif;
             font-size: 20px;
         }
+
+        /* input Format */
         input[type=text], input[type=password], input[type=email],select {
             font-family: Georgia, serif;
             font-size: 20px;
@@ -80,32 +84,9 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script>
     
-    var x = document.getElementById("please");
-        /*
-        var l = {};
-        console.log(l);
-        $.ajax({
-            url:"usolog.php",
-            method:"post",
-            data: l,
-            success: showPosition(res){
-                console.log(res);
-            }
-        })
-        */
-       /*
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(location); 
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-function location(position){
-    $url = 'usolog.php';
-    $.get($url,{long_coords:position.coords.longitude, lat_coords:position.coords.latitude});
-}
-*/
+    var x = document.getElementById("please");  
+        
+    
 const options = {
   enableHighAccuracy: true,
   timeout: 5000,
@@ -121,15 +102,13 @@ function success(pos) {
   var d = crd.latitude;
   var c = crd.longitude;
 
-  /*
-  $url = 'usolog.php';
-    $.get($url,long_coords:"wahh", lat_coords:"pls");
-    */
+ 
 }
 
+//find any errors with account creation
 function error(err) {
     document.getElementById("please").innerHTML = `ERROR(${err.code}): ${err.message}`;
-  //document.getElementById(`ERROR(${err.code}): ${err.message}`).innerHTML = ;
+  
 }
 
 function getLocation() {
@@ -140,27 +119,29 @@ function getLocation() {
   }
 }
 </script>
+<!-- header -->
     <h1>Hidden Treasures</h1>
     <div>
+        <!-- form for account creation -->
         <form action="usolog.php" method="post" enctype = "multipart/form-data" novalidate>
-            <label for="email">Email:</label>
+            <label for="email">Email:</label>   <!-- input email -->
             <input type="email" id="email" name="email" placeholder="Unused Email..."><br>
-            <label for="username">Username:</label>
+            <label for="username">Username:</label> <!-- input username -->
             <input type="text" id="username" name="username" placeholder="Unused Username..."><br>
-            <label for="password">Password:</label>
+            <label for="password">Password:</label> <!-- input password -->
             <input type="password" id="password" name="password" placeholder="Password..." ><br>
-            <label for="cpassword">Confirm Password:</label>
+            <label for="cpassword">Confirm Password:</label>    <!-- confirm password -->
             <input type="password" id="cpassword" name="cpassword" placeholder="CONFIRM" ><br>
-            <label type="text">Bio</label>
+            <label type="text">Bio</label>  <!-- input bio -->
             <input type="text" id="bio" name="bio" placeholder="Write something about yourself!"><br>
-            <label for="file">Please upload a profile picture</label>
+            <label for="file">Please upload a profile picture</label>   <!-- upload profile picture -->
             <input type="file" name= "file">
             <input type="submit" value="Submit" name="location" onclick="getLocation()">
-            <p>Already have an account? <a href="index.php"> Log in!</a></p>
+            <p>Already have an account? <a href="index.php"> Log in!</a></p>    <!-- goes to login -->
         </form>
     </div>
+    <!-- header -->
     <h2> Account Creation </h2>
-    <h2>Logo Here</h2>
     
 </body>
 </html>

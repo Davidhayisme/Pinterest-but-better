2d1ce80868fcea72951a778db1497f95479d6abb
David Hay <113220253+Davidhayisme@users.noreply.github.com>
2022-12-09 06:37:59 -0500

    Documented connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * @var mixed $host The DNS name of hosting service
+ */
 $host = "actualtesting.cwqrcac9nt4i.us-east-1.rds.amazonaws.com";
+//Database name
                 $dbname = "images";
+                //Username
                 $usernames = "admin";
+                //Password
                 $passwords = "Pinterestbutbetter";
+                //Actual connection to the database
                 $conn = mysqli_connect($host,$usernames,$passwords,$dbname);
+                //Connection error checking
                 if(mysqli_connect_errno()){
                     die("Connection error: " . mysqli_connect_error());
                 }
+                //Returning the connection to the database
 return $conn;

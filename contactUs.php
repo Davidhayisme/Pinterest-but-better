a0930770c78a63e416380cbe82b54b3bcddf0a3b
ErinLi1998! <70736136+ErinLi1998@users.noreply.github.com>
2022-12-09 21:04:37 -0500

    Add files via upload

---
+++ contactUs.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">    
+        <style>
+        .header {
+            background-color: #57427e;
+            text-align: center;
+            padding: 20px;
+            font-family: "Sofia", sans-serif;
+        }
+        .header h1 {
+            color: #eeeeee;
+            font-size: 30px;
+            font-family: "Sofia", sans-serif;
+        }
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #4D5F0F;
+            font-family: Georgia, serif;
+        }
+        li {
+            float: left;
+        }
+        li a {
+            display: block;
+            padding: 8px;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-family: Georgia, serif;
+        }
+        
+        li a:hover {
+            background-color: #52554a;
+        }
+        .parent {
+            display: flex;
+        }
+        .active {
+            background-color: #4D5F0F;
+        }
+        h2 {
+            color: black;
+            font-size: 25px;
+            font-family: Georgia, serif;
+        }
+        a1:link {
+            color: black;
+            font-size: 20px;
+            font-family: Georgia, serif;
+            text-decoration: none;
+        }
+      
+         
+        h3 {
+            color: black;
+            font-size: 15px;
+            font-family: Georgia, serif;
+            text-decoration: none;
+        }
+        h3:hover {
+            color: #4D5F0F;
+            text-decoration: underline;
+        }
+        h4 {
+            color: black;
+            font-size: 15px;
+            font-family: Georgia, serif;
+        }
+        div1{
+            padding-top: 30px;
+            padding-left: 30px;
+        }
+        div2{
+            padding-top: 50px;
+            padding-left: 30px;
+        }
+        :root {
+            --username: JohnDoe22;
+        }
+
+        div.gallery {
+            margin: 20px 10px 10px 10px;
+            float: left;
+            width: 180px;
+        }
+
+        div.gallery:hover {
+            opacity: 0.6;
+        }
+
+        div.gallery img {
+            width: 100%;
+            height: auto;
+        }
+        </style>
+    </head>
+    <body>
+        <div class="header">
+            <h1>Hidden Treasures</h1>
+        </div>
+        <ul>
+        <li><a href="homePage.html">Home</a></li>
+        <li style="float:right"><a class="active" href="#userProfile">Conntact US</a></li>
+        <li style="float:right"><a class="active" href="#profile">Profile</a></li>
+        <li style="float:right"><a class="active" href="posting.html">Post</a></li>
+        <li style="float:right"><a class="active" href="#filter">Filter</a></li>
+        </ul>
+        <h2>having questions ? </h2>
+        <h3>contact us</h3>
+        <p>
+            <br>
+        Our address : <a href="https://www.bing.com/search?q=muhlenberg+college&cvid=a831196d1cc74acdb73feecca29d64ca&aqs=edge.1.69i57j46j0l3j46j0l3.2473j0j1&pglt=297&FORM=ANNTA1&PC=U531&mkt=zh-CN">
+            <br>        
+            2400 chew street, ALLENTOWN PA,18104
+        </a>
+          <br>
+
+        </p>
+        <h3>Email : example@gmail.com</h3>
+        <h3>call# : 123-456-8888</h3>
+
+
+    </body> 
+    
+    
+     
+    
+    
+</html> 
